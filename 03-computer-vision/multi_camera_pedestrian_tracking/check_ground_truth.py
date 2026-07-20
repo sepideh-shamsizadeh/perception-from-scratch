ec10525716a4361a804e0a46e2e4b30d2c6c480b
sepid <sepideh.shamsizadeh@gmail.com>
2026-07-20 15:23:47 +0200

    Implement image reading and bounding-box drawing for ground truth check

--- 03-computer-vision/multi_camera_pedestrian_tracking/check_ground_truth.py
+++ 03-computer-vision/multi_camera_pedestrian_tracking/check_ground_truth.py
@@ -1,35 +1,58 @@
 from pathlib import Path
 import json
+import cv2
 
 
 BASE = Path(__file__).parent / "Wildtrack_dataset"
-Image_dir = BASE / "image_subsets"
+Image_dir = BASE / "Image_subsets"
 annotations_dir = BASE / "annotations_positions"
 calibration_dir = BASE / "calibrations"
 
-def get_box(frame, camera_num):
-    with open(annotations_dir/ (frame +".json"), "r", encoding="utf-8") as file:
-        data = json.load(file)
-    
-    cam_fame = data[0]["views"][camera_num]
+def get_box(cam_frame):
     bbox = []
-    if(cam_fame["xmax"] >0):
-        bbox = [cam_fame["xmax"],cam_fame["xmin"], cam_fame["ymax"], cam_fame["ymin"]]
-        return bbox
+    if(cam_frame["xmax"] >0):
+        bbox = [cam_frame["xmin"],cam_frame["ymin"], cam_frame["xmax"], cam_frame["ymax"]]
+    return bbox
 
-def draw_box(boxes, image):
-    #TODO: draw boxs on the given image
-    pass
 
-def get_image(camera_num, frame):    
-    #TODO: read image for each camera numbera and file name
-    pass
+def color_for_person(person_id):
+    blue = (person_id*37)% 256
+    green = (person_id*67)% 256
+    red = (person_id*97)%256
+
+    return blue, green, red
+
+
+def draw_box(boxes, image, color, id):
+    if(len(boxes)>0):
+        image = cv2.rectangle(image, (boxes[0], boxes[1]), (boxes[2],boxes[3]), color, 2)
+        image = cv2.putText(image, str(id), (boxes[0]-1, boxes[1]-1), cv2.FONT_HERSHEY_COMPLEX, 1, color, 1)
+        
+    return image
+    
+
+def get_image(camera_num, image_name):    
+    camera_path = "C"+ str(camera_num)
+    image = cv2.imread(Image_dir/camera_path/image_name)
+
+    return image
 
 def read_annotation_file():
     with open(annotations_dir/"00000000.json", "r", encoding="utf-8") as file:
         data = json.load(file)
     print(len(data))
-    print(get_box("00000000", 1))
+    image = get_image(1, "00000000.png")
+
+    for i in range(0, len(data)):
+        id = data[i]["personID"]
+        cam_frame = data[i]["views"][0]
+        
+        boxes= get_box(cam_frame)
+        color = color_for_person(id)
+        image = draw_box(boxes, image, color, id)
+
+    cv2.imshow("00000000", image)
+    cv2.waitKey(0)
 
 
 read_annotation_file()
